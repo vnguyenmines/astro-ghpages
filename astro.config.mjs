cd21b09cac7eeffe4de8d1f4ad8caf56c8bb82af
vincent <vincent.nguyennf@gmail.com>
2024-12-18 12:39:43 -0700

    Astro config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://vnguyenmines.github.io',
+    base: "astro-ghpages",
 });
